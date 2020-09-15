5261b4dd55ddc0c64785bba3a10468bbb5861bfb
TiaanSmal98 <tiaans@emperiogroup.co.za>
2020-09-15 17:27:30 +0200

    Keys class containing Dictionaries added to assist in identifying Audio clip settings based on an integer stored in the JSON settings file

---
+++ Configurable Asset Importer/Assets/Asset Importer/Models/Keys/Keys.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class Keys
+{
+    public static Dictionary<int, AudioCompressionFormat> AudioCompressionFormats = new Dictionary<int, AudioCompressionFormat> {
+        {1, AudioCompressionFormat.AAC },
+        {2, AudioCompressionFormat.ADPCM },
+        {3, AudioCompressionFormat.ATRAC9 },
+        {4, AudioCompressionFormat.GCADPCM },
+        {5, AudioCompressionFormat.HEVAG },
+        {6, AudioCompressionFormat.MP3 },
+        {7, AudioCompressionFormat.PCM },
+        {8, AudioCompressionFormat.VAG },
+        {9, AudioCompressionFormat.Vorbis },
+        {10,AudioCompressionFormat.XMA }
+    };
+
+    public static Dictionary<int, AudioClipLoadType> AudioClipLoadTypes = new Dictionary<int, AudioClipLoadType>
+    {
+        {1, AudioClipLoadType.CompressedInMemory },
+        {2, AudioClipLoadType.DecompressOnLoad },
+        {3, AudioClipLoadType.Streaming }
+    };
+}
