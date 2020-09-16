a8d5cfd99591e589a640d13b5ed277279dedb28a
TiaanSmal98 <tiaans@emperiogroup.co.za>
2020-09-16 17:28:36 +0200

    Implemented IActiveBuildTargetChanged to automatically import JSON settings when the project type changes

---
+++ Configurable Asset Importer/Assets/Asset Importer/AutomaticImportOnBuildChange.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build;
+
+public class AutomaticImportOnBuildChange : IActiveBuildTargetChanged
+{
+    public int callbackOrder { get { return 0; } }
+
+    public void OnActiveBuildTargetChanged(BuildTarget previousTarget, BuildTarget newTarget)
+    {
+        ApplySettings.ImportAndApplySettings();
+    }
+}
